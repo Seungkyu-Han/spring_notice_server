c470852524b1a22ed715f9223b0231fcd37fd4f2
Seungkyu-Han <trust1204@gmail.com>
2024-10-30 14:37:30 +0900

    feat: add sns router

---
+++ src/main/kotlin/org/seungkyu/notice/router/SNSRouter.kt
@@ -0,0 +1,19 @@
+package org.seungkyu.notice.router
+
+import org.seungkyu.notice.service.SnsService
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration
+class SNSRouter {
+
+    @Bean
+    fun snsRouters(
+        snsService: SnsService
+    ) = coRouter{
+        "/sns".nest {
+            POST("", snsService::publish)
+        }
+    }
+}
