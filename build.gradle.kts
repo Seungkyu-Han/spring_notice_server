3ba357ba5b7238d0dd3486037f9f25830b34e71a
Seungkyu-Han <trust1204@gmail.com>
2024-10-30 14:13:17 +0900

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     kotlin("jvm") version "2.0.0"
+    kotlin("plugin.spring") version "1.9.25"
+    id("org.springframework.boot") version "3.3.4"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "seungkyu"
@@ -10,7 +13,19 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-webflux:3.3.5")
+    //AWS SNS
+    implementation(platform("io.awspring.cloud:spring-cloud-aws-dependencies:3.0.1"))
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-sqs")
+    implementation("io.awspring.cloud:spring-cloud-aws-starter-sns")
+
+
+    //LOMBOK
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok")
+
+
+
 }
 
 tasks.test {
